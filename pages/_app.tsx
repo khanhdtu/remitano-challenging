a99e27df6452c43a57c0b41deb683f0c2463db98
nasa <nasa@vstudios.com>
2022-04-22 15:58:12 +0700

    add theme-ui, react-query, react-hot-toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { ThemeProvider } from 'theme-ui'
+import { Toaster } from 'react-hot-toast'
+import { theme } from './theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { refetchOnWindowFocus: false },
+  },
+})
+
+function RemitanoChallengingApplication({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+          <Toaster />
+        </ThemeProvider>
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
-export default MyApp
+export default RemitanoChallengingApplication
---
+++ pages/theme.ts
@@ -0,0 +1,13 @@
+import { Theme } from 'theme-ui'
+
+export const theme: Theme = {
+  breakpoints: [],
+  fonts: {},
+  colors: {},
+  sizes: {},
+  images: {},
+  styles: {},
+  forms: {},
+}
+
+export default theme

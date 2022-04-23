c14bc5c5bf096f15d75705a296a55176e1ed087c
nasa <nasa@vstudios.com>
2022-04-23 15:09:18 +0700

    add layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,12 @@
-import styles from '@styles/Home.module.css'
 import type { NextPage } from 'next'
-import { useCurrentUser } from '@services/queries'
-import { useFetchCurrentUserMutation } from '@services/mutations'
 import { Box, Flex } from 'theme-ui'
-import { useEffect } from 'react'
-import { Header } from '@components/Header'
+import Layout from '@pages/layout'
 
 const Home: NextPage = () => {
-  const { mutate: onFetchUser } = useFetchCurrentUserMutation()
-  const { data: user } = useCurrentUser()
-  console.log('userxxx', user)
-  useEffect(() => onFetchUser(), [])
-
   return (
-    <Box className={styles.container}>
-      <Header user={user} />
-      <Flex></Flex>
-    </Box>
+    <Layout>
+      <Flex>Hello</Flex>
+    </Layout>
   )
 }
 
---
+++ pages/layout.tsx
@@ -0,0 +1,24 @@
+import styles from '@styles/Home.module.css'
+import type { NextPage } from 'next'
+import { useCurrentUser, useGetVideos } from '@services/queries'
+import { useFetchCurrentUserMutation } from '@services/mutations'
+import { Box } from 'theme-ui'
+import { useEffect } from 'react'
+import { Header } from '@components/Header'
+
+const Layout: NextPage = props => {
+  const { mutate: onFetchUser } = useFetchCurrentUserMutation()
+  const { data: user } = useCurrentUser()
+  const { data: videos } = useGetVideos()
+
+  useEffect(() => onFetchUser(), [])
+
+  return (
+    <Box className={styles.container}>
+      <Header user={user} />
+      {props.children}
+    </Box>
+  )
+}
+
+export default Layout
